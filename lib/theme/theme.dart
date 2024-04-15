74a9db432cfa06a712d644eb3891ca119163d426
Ahmad Owais <okams7@gmail.com>
2024-04-15 17:02:02 +0200

    adding more themes

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_setup/theme/appbar_theme.dart';
+import 'package:flutter_project_setup/theme/elevated_button_theme.dart';
 import 'package:flutter_project_setup/theme/text_theme.dart';
 
 ThemeData lightMode = ThemeData(
   useMaterial3: true,
   brightness: Brightness.light,
   primaryColor: Colors.blue[500],
-  textTheme: lightTextTheme,
   scaffoldBackgroundColor: Colors.white,
+  textTheme: lightTextTheme,
+  elevatedButtonTheme: lightElevatedButtonTheme,
+  appBarTheme: lightAppBarTheme,
 );
 
 ThemeData darkMode = ThemeData(
   useMaterial3: true,
   brightness: Brightness.dark,
   primaryColor: Colors.blue[800],
-  textTheme: darkTextTheme,
   scaffoldBackgroundColor: Colors.black87,
+  textTheme: darkTextTheme,
+  elevatedButtonTheme: darkElevatedButtonTheme,
+  appBarTheme: darkAppBarTheme,
 );
